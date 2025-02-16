ed8186cfc4604c57588ea9b71878becbc2e971e9
ozalperen <alperenoz93@gmail.com>
2025-02-16 17:31:44 +0300

    Update audio model to use 'large.pt' for improved transcription accuracy

--- TranscriberModels.py
+++ TranscriberModels.py
@@ -11,7 +11,7 @@ def get_model(local):
 
 class WhisperTranscriber:
     def __init__(self):
-        self.audio_model = whisper.load_model(os.path.join(os.getcwd(), 'tiny.en.pt'))
+        self.audio_model = whisper.load_model(os.path.join(os.getcwd(), 'large.pt'))
         print(f"[INFO] Whisper using GPU: " + str(torch.cuda.is_available()))
 
     def get_transcription(self, wav_file_path):
